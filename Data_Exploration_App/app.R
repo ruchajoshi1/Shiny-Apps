dd5a60d69ec656554d64564d35cd55b6e98febea
ruchajoshi1 <ruchajoshim@gmail.com>
2018-09-20 11:12:00 -0400

    Shiny App examples

---
+++ Data_Exploration_App/app.R
@@ -0,0 +1,94 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(gapminder)
+library(colourpicker)
+library(DT)
+
+my_css <- "
+#download_data {
+/* Change the background colour of the download button
+to orange. */
+background: orange;
+
+/* Change the text size to 20 pixels. */
+font-size: 20px;
+}
+
+#table {
+/* Change the text colour of the table to red. */
+color: red;
+}
+"
+
+ui <- fluidPage(
+  h1("Gapminder"),
+  # Add the CSS that we wrote above to the Shiny app
+  tags$style(my_css),
+  tabsetPanel(
+    tabPanel(
+      title = "Inputs",
+      sliderInput(inputId = "life", label = "Life expectancy",
+                  min = 0, max = 120,
+                  value = c(30, 50)),
+      selectInput("continent", "Continent",
+                  choices = c("All", levels(gapminder$continent))),
+      downloadButton("download_data")
+    ),
+    tabPanel(
+      title = "Plot",
+      plotOutput("plot")
+    ),
+    tabPanel(
+      title = "Table",
+      DT::dataTableOutput("table")
+    )
+  )
+)
+
+server <- function(input, output) {
+  filtered_data <- reactive({
+    data <- gapminder
+    data <- subset(
+      data,
+      lifeExp >= input$life[1] & lifeExp <= input$life[2]
+    )
+    if (input$continent != "All") {
+      data <- subset(
+        data,
+        continent == input$continent
+      )
+    }
+    data
+  })
+  
+  output$table <- DT::renderDataTable({
+    data <- filtered_data()
+    data
+  })
+  
+  output$download_data <- downloadHandler(
+    filename = "gapminder_data.csv",
+    content = function(file) {
+      data <- filtered_data()
+      write.csv(data, file, row.names = FALSE)
+    }
+  )
+  
+  output$plot <- renderPlot({
+    data <- filtered_data()
+    ggplot(data, aes(gdpPercap, lifeExp)) +
+      geom_point() +
+      scale_x_log10()
+  })
+}
+
+shinyApp(ui, server)
